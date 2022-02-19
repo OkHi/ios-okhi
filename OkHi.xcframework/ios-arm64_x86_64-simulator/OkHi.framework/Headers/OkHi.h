e227027c614861b8d16f0e0f7cb67ecc01437eb6
Julius Kiano <jgkiano@gmail.com>
2022-02-19 08:48:27 +0300

    initial commit

---
+++ OkHi.xcframework/ios-arm64_x86_64-simulator/OkHi.framework/Headers/OkHi.h
@@ -0,0 +1,18 @@
+//
+//  OkHi.h
+//  OkHi
+//
+//  Created by Julius Kiano on 18/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OkHi.
+FOUNDATION_EXPORT double OkHiVersionNumber;
+
+//! Project version string for OkHi.
+FOUNDATION_EXPORT const unsigned char OkHiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OkHi/PublicHeader.h>
+
+

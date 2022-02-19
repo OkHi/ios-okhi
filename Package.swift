45cc186b434d6ce913beb1a610ecc3a3416f0d05
Julius Kiano <jgkiano@gmail.com>
2022-02-19 10:06:59 +0300

    update manifest

--- Package.swift
+++ Package.swift
@@ -16,14 +16,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "OkHi",
-            path: "OkHi.xcframework",
-            dependencies: []),
-        .testTarget(
-            name: "OkHiTests",
-            dependencies: ["OkHi"]),
+            path: "./OkHi.xcframework")
     ]
 )

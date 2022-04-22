4cc113cca8e378eb3161ada5f94c46d71badd6e4
Julius Kiano <jgkiano@gmail.com>
2022-04-22 08:35:08 +0300

    added device ping feature

--- OkHi.xcframework/ios-arm64_x86_64-simulator/OkHi.framework/Headers/OkHi-Swift.h
+++ OkHi.xcframework/ios-arm64_x86_64-simulator/OkHi.framework/Headers/OkHi-Swift.h
@@ -446,6 +446,13 @@ SWIFT_CLASS("_TtC4OkHi8OkVerify")
 - (void)startMonitoring;
 @end
 
+
+SWIFT_CLASS("_TtC4OkHi17OkVerifyAnalytics")
+@interface OkVerifyAnalytics : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (void)startAnalytics;
+@end
+
 @class NSEntityDescription;
 @class NSManagedObjectContext;
 
@@ -953,6 +960,13 @@ SWIFT_CLASS("_TtC4OkHi8OkVerify")
 
 @interface OkVerify (SWIFT_EXTENSION(OkHi))
 - (void)startMonitoring;
+@end
+
+
+SWIFT_CLASS("_TtC4OkHi17OkVerifyAnalytics")
+@interface OkVerifyAnalytics : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (void)startAnalytics;
 @end
 
 @class NSEntityDescription;
